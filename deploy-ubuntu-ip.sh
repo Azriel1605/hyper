8f3ef117351109cf5593ae660d40a6d3718bb12a
ahmad <ahmad.qeis122@gmail.com>
2025-10-17 12:58:16 +0700

    a

--- deploy-ubuntu-ip.sh
+++ deploy-ubuntu-ip.sh
@@ -16,9 +16,9 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Konfigurasi
-APP_DIR="/var/www/hdsc-app"
-APP_USER="hdsc"
-APP_GROUP="hdsc"
+APP_DIR="/var/www/hyper-app"
+APP_USER="hyper"
+APP_GROUP="hyper"
 VENV_DIR="$APP_DIR/venv"
 
 # Dapatkan IP VPS
